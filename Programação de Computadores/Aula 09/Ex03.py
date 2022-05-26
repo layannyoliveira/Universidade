a8b5a1ea21aee8f1fd3d388002ebfa637cbc3953
layannyoliveira <102493129+layannyoliveira@users.noreply.github.com>
2022-05-26 17:50:47 -0300

    up

--- Programação de Computadores/Aula 09/Ex03.py
+++ Programação de Computadores/Aula 09/Ex03.py
@@ -1,9 +1,28 @@
 contador = 0
 contador2 = 0
+contador3 = 0
 soma = 0
-
+soma2 = 0
+idade = 0
+midade = 0
+salario = 0
 while True:
-    res= input("Acabaram os habitantes?")
-    if res =="s" or res == "S":
+    idade= int("Informe a idade?")
+    if idade < 0:
         break
-    
+    soma = soma + idade
+    contador = contador + 1
+    salario = float(input("Qual o seu salário?"))
+    if salario < 600:
+        contador2 = contador2 + 1
+    if idade >40:
+        contador3 = contador3 +1
+        soma2 = soma2 + salario
+
+msalario = soma2/contador3       
+midade = soma/contador
+midade2 = soma2/contador3
+
+print("Media da idade do grupo", midade)
+print("Media dos salario dos habitantes com mais 40 anos", msalario)
+print("Quantidade de pessoas que recebem menos de 600 reais", contador2)
